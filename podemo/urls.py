3edea67b8c38e0af4e7add8b1bc4186a1bdec636
chas <chas@localhost.localdomain>
2015-02-13 13:53:30 -0800

    trying again

--- podemo/urls.py
+++ podemo/urls.py
@@ -5,12 +5,15 @@ from django.conf.urls.static import static
 
 #admin.autodiscover()
 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'podemo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    (r'^$', 'django.views.static.serve', {'document_root':'/var/djapps/podemo/','path':'index.html'}),
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^organization/', include('organization.urls', namespace="organization")),
 
-)# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
